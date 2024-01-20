59978b5a740ffac0c71e3cf327ff8b4459fe9134
Joao Teixeira <jaobacanao@gmail.com>
2024-01-19 23:57:57 -0300

    feat: implemeting writible streams on server

--- streams/stream-server/stream-http-server.js
+++ streams/stream-server/stream-http-server.js
@@ -3,16 +3,26 @@ import { Transform } from 'node:stream';
 
 class NegateStream extends Transform {
     _transform(chunk, encoding, callback) {
-        const transformed = (Number(chunk.toString() * -1));
+        const transformed = Number(chunk.toString() * -1);
         console.log(transformed);
 
         callback(null, Buffer.from(String(transformed)));
     }
 };
 
+class MultiplyByTwoStream extends Writable {
+
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 2);
+        callback();
+    }
+}
+
+
 const server = http.createServer((req, res) => {
     req.on('data', (chunk) => chunk)
         .pipe(new NegateStream())
+        .pipe(new MultiplyByTwoStream())
         .pipe(res)
 });
 
